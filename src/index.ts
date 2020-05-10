0c10fc0ebb196d7884bd14fa819b7bfc6cb6e24e
Ricardo Dias Cavalcante <ricardodc@gmail.com>
2020-05-10 03:44:32 -0300

    Adding orbit controls

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,111 @@
-import {Scene, PerspectiveCamera, WebGLRenderer,
-         Mesh, MeshBasicMaterial,
-          MeshPhongMaterial, AmbientLight, PointLight,
-           Color } from 'three';
-import { Stats } from 'three-stats' ;
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Mesh,
+  MeshBasicMaterial,
+  MeshPhongMaterial,
+  AmbientLight,
+  PointLight,
+  Color,
+} from "three";
+import { Stats } from "three-stats";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+class Main {
+  scene: Scene = new Scene();
+  stats = new Stats();
+  camera: PerspectiveCamera = new PerspectiveCamera(
+    35,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer: WebGLRenderer = new WebGLRenderer({
+    canvas: <HTMLCanvasElement>document.getElementById("myCanvas"),
+    antialias: true,
+  });
 
-class Main{
-    scene:Scene = new Scene();
-    stats = new Stats();
-    camera:PerspectiveCamera = new PerspectiveCamera(35, window.innerWidth/window.innerHeight, 0.1, 1000);
-    renderer : WebGLRenderer = new WebGLRenderer({canvas:<HTMLCanvasElement>document.getElementById("myCanvas"), antialias:true});
-    
-    material : MeshBasicMaterial = new MeshBasicMaterial({ color : 0x00f900 , wireframe: true});
-    materialPhong : MeshPhongMaterial = new MeshPhongMaterial({ color : 0x0000ff });
-    
-    monkeyMesh:Mesh;
+  material: MeshBasicMaterial = new MeshBasicMaterial({
+    color: 0x00f900,
+    wireframe: true,
+  });
+  materialPhong: MeshPhongMaterial = new MeshPhongMaterial({ color: 0x0000ff });
 
-    pointLight : PointLight;
-    
-    constructor(){
-        this.configurePointLight();
-        
-        this.scene.add(this.pointLight);
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.gammaOutput = true;
-        this.renderer.gammaFactor = 2.2;
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.scene.add(new AmbientLight(new Color(0.2,0.2,0.2).getHex()));
-        this.configureCamera();
+  monkeyMesh: Mesh;
 
-        this.loadModelGLTF();
-        document.body.appendChild(this.stats.dom);
-    }
+  pointLight: PointLight;
+  controls: OrbitControls = new OrbitControls(
+    this.camera,
+    this.renderer.domElement
+  );
 
-    loadModelGLTF() {
-        let gltfLoader = new GLTFLoader();
-        gltfLoader.load('assets/webmonkey.glb',
-            (gltf)=>{
-                this.monkeyMesh = <Mesh>gltf.scene.children[0];
-                this.monkeyMesh.position.x = 0;
-                this.monkeyMesh.position.y= 0;
-                this.monkeyMesh.position.z = 0;
-                
-                this.scene.add(this.monkeyMesh);
-            });
-        
-    }
+  constructor() {
+    this.configurePointLight();
 
-    configurePointLight(){
-        this.pointLight = new PointLight();
-        this.pointLight.position.set(5,5,5);
-        this.pointLight.intensity = 0.8;
-    }
+    this.scene.add(this.pointLight);
+    this.scene.add(new AmbientLight(new Color(0.2, 0.2, 0.2).getHex()));
 
-    configureCamera(){
-        this.camera.position.z = 5;
-        this.camera.lookAt(0,0,0);
-        
-    }
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.gammaFactor = 2.2;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    //configure Camera
+    this.configureCamera();
+    //Load model
+    this.loadModelGLTF();
+    //Configure controls
+    this.configureControls();
 
-    render(){
-        this.stats.begin();
-        
-        //While not loaded
-        if(this.monkeyMesh) {
-            this.monkeyMesh.rotation.y -= 0.01;
-        }
-        this.renderer.render(this.scene, this.camera); 
-        this.stats.end();
-        requestAnimationFrame(()=>this.render());
+    document.body.appendChild(this.stats.dom);
+  }
 
-    }
+  configureControls() {
+    this.controls.addEventListener("change", this.render);
+    this.controls.minDistance = 10;
+    this.controls.maxDistance = 50;
+    this.controls.enablePan = false;
+  }
 
-    start(){
-        this.render();
-    }
+  loadModelGLTF() {
+    let gltfLoader = new GLTFLoader();
+    gltfLoader.load("assets/webmonkey.glb", (gltf) => {
+      this.monkeyMesh = <Mesh>gltf.scene.children[0];
+      this.monkeyMesh.position.x = 0;
+      this.monkeyMesh.position.y = 0;
+      this.monkeyMesh.position.z = 0;
+
+      this.scene.add(this.monkeyMesh);
+    });
+  }
+
+  configurePointLight() {
+    this.pointLight = new PointLight();
+    this.pointLight.position.set(5, 5, 5);
+    this.pointLight.intensity = 0.8;
+  }
+
+  configureCamera() {
+    this.camera.position.z = 15;
+    this.camera.lookAt(0, 0, 0);
+  }
+
+  render() {
+    this.stats.begin();
+
+    //While not loaded
+    // if (this.monkeyMesh) {
+    //   this.monkeyMesh.rotation.y -= 0.01;
+    // }
+    this.renderer.render(this.scene, this.camera);
+    this.stats.end();
+    requestAnimationFrame(() => this.render());
+  }
+
+  start() {
+    this.render();
+  }
 }
-
 
 const main = new Main();
 main.start();
-
-
-
